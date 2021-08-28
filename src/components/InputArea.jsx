c322d9dea6e28dcacd7d5de44f791d675e194efd
setykarimi <setykarimi80@gmail.com>
2021-08-28 15:46:08 +0430

    create new component for InputArea

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,27 @@
+import React ,{useState} from 'react';
+
+function InputArea(props) {
+    const [inputText, setInputText] = useState("");
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+   return(
+        <div className="form">
+        <input type="text"
+            onChange={handleChange}
+            value={inputText}
+        />
+        <button onClick={()=>{
+            props.onAdd(inputText);
+            setInputText("");
+        }}>
+            <span>Add</span>
+        </button>
+    </div>
+   )
+
+}
+export default InputArea;
