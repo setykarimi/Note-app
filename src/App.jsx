a599c6b1a6016fbd4932bb7fe15d267852db8b71
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-24 16:18:36 +0330

    Subject: Note app, Description: Add sidebar, Date: 9/24/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import NoteList from "./components/noteList";
+import Sidebar from "./components/sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container p-4 mx-auto grid grid-cols-4">
+      <Sidebar />
+      <div className="col-span-3">
+      <NoteList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
