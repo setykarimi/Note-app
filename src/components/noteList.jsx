906b36d76de7a18e918131ed22f10edfd7e8d853
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-24 16:18:54 +0330

    Subject: Note app, Description: Add note item, Date: 9/24/2023

--- src/components/noteList.jsx
+++ src/components/noteList.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import NoteItem from './noteItem'
 
 export default function NoteList() {
   return (
-    <div>NoteList</div>
+    <div>
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+    </div>
   )
 }
