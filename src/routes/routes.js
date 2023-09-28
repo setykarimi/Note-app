96bd01b737f2d9092c733424c01708493f4a69df
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 10:15:40 +0330

    Subject: Note app, Description: Create new folder for routes, Date: 9/28/2023

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/layout";
+import TodayTask from "../pages/todayTasks";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <TodayTask />,
+      },
+    ],
+  },
+]);
