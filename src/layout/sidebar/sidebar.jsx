310167d3036c75d39557e5af9b0dfd646ccf3259
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 11:10:43 +0330

    Subject: Note app, Description: Edit style and add new component, Date: 9/28/2023

--- src/layout/sidebar/sidebar.jsx
+++ src/layout/sidebar/sidebar.jsx
@@ -4,10 +4,11 @@ import SearchBar from "./components/searchBar";
 import Tasks from "./components/tasks";
 import Lists from "./components/lists";
 import Tags from "./components/tags";
+import OtherLinks from "./components/otherlinks";
 
 export default function Sidebar() {
   return (
-    <div className="bg-gray-100 p-6 rounded-xl">
+    <div className="bg-gray-100 p-6 rounded-xl sidebar h-full flex flex-col">
       <div className="title flex justify-between items-center">
         <span className="font-bold text-2xl text-gray-600">Menu</span>
         <button>
@@ -18,6 +19,7 @@ export default function Sidebar() {
       <Tasks />
       <Lists />
       <Tags />
+      <OtherLinks />
     </div>
   );
 }
