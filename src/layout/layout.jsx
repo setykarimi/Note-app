70d81d5287c3d0222865ffb6773fedef58d04e52
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 16:47:44 +0330

    Subject: Note app, Description: Edit Layout for responsive view, Date: 9/28/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -3,16 +3,16 @@ import Sidebar from "./sidebar/sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  const mobileView =  window.innerWidth < 1025
+  const mobileView = window.innerWidth < 1025;
   const [expanded, setExpanded] = useState(!mobileView);
   return (
     <div
       className={`xl:container mx-auto  ${
         expanded ? "md:grid md:grid-cols-4" : "flex"
-      } gap-8 p-4 transition-all`}
+      } md:gap-8 gap-4 p-4 transition-all`}
     >
       <Sidebar expanded={expanded} setExpanded={setExpanded} />
-      <div className="col-span-3">
+      <div className={`${expanded ? 'col-span-3' : "w-full"}`}>
         <Outlet />
       </div>
     </div>
