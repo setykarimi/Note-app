198a720eb450108d175ff85c6fde1463cb133be0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 12:04:40 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 9/28/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
-import Sidebar from './sidebar/sidebar'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from "react";
+import Sidebar from "./sidebar/sidebar";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  const [expanded, setExpanded] = useState(true);
+  const mobileView =  window.innerWidth < 1025
+  const [expanded, setExpanded] = useState(!mobileView);
+  console.log(window.innerWidth);
+  console.log(mobileView);
   return (
-    <div className={`lg:container mx-auto  ${expanded ? 'grid grid-cols-4' : "flex" } gap-8 p-4 transition-all`}>
-        <Sidebar expanded={expanded} setExpanded={setExpanded}/>
-     <div className='col-span-3'>
-     <Outlet />
-     </div>
+    <div
+      className={`xl:container mx-auto  ${
+        expanded ? "md:grid md:grid-cols-4" : "flex"
+      } gap-8 p-4 transition-all`}
+    >
+      <Sidebar expanded={expanded} setExpanded={setExpanded} />
+      <div className="col-span-3">
+        <Outlet />
+      </div>
     </div>
-  )
+  );
 }
