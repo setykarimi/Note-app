bff90361297cc89d3be10bac051d26f1c92f770e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 11:54:23 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 9/28/2023

--- src/layout/sidebar/components/lists.jsx
+++ src/layout/sidebar/components/lists.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
 import { IoMdAdd } from "react-icons/io";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Lists() {
+export default function Lists({ expanded }) {
   const { pathname } = useLocation();
 
   const lists = [
@@ -27,45 +27,59 @@ export default function Lists() {
   ];
   return (
     <div className="mt-4 border-b border-b-gray-200 pb-4">
-      <span className="text-xs text-gray-600 font-bold">LISTS</span>
+      <span
+        className={`text-xs text-gray-600 font-bold block ${
+          expanded ? "text-left" : "text-center"
+        }`}
+      >
+        LISTS
+      </span>
       <div className="flex flex-col  mt-2">
         {lists.map((list) => (
-          <CustomLink list={list} currentPath={pathname} />
+          <CustomLink list={list} currentPath={pathname} expanded={expanded} />
         ))}
-        <button className="flex items-center gap-2 text-gray-600 font-medium px-1 py-2">
+        <button
+          className={`flex items-center gap-2 text-gray-600 font-medium px-1 py-2 ${
+            !expanded ? " justify-center" : ""
+          }`}
+        >
           <IoMdAdd className="text-gray-500" size={20} />
-          Add New List
+          {expanded && "Add New List"}
         </button>
       </div>
     </div>
   );
 }
 
-const CustomLink = ({ list, currentPath }) => {
+const CustomLink = ({ list, currentPath, expanded }) => {
   const path = list.to == currentPath;
 
   return (
     <Link
       to={list.to}
       className={`${
-        path && "bg-gray-200 font-bold"
-      } p-2 rounded-md text-gray-600 flex items-center`}
+        path ? "bg-gray-200 font-bold" : ""
+      } p-2 rounded-md text-gray-600 flex items-center ${
+        !expanded ? "justify-center" : ""
+      }`}
     >
       <span className="flex items-center gap-4">
         <span
           className={`block ${list.color} w-3 h-3`}
           style={{ borderRadius: "2px" }}
         ></span>
-        {list.name}
+        {expanded && list.name}
       </span>
 
-      <span
-        className={`block ml-auto ${
-          path ? "bg-white" : "bg-gray-200"
-        } font-bold w-8 text-center py-[2px] rounded text-xs`}
-      >
-        {list.badge}
-      </span>
+      {expanded && (
+        <span
+          className={`block ml-auto ${
+            path ? "bg-white" : "bg-gray-200"
+          } font-bold w-8 text-center py-[2px] rounded text-xs`}
+        >
+          {list.badge}
+        </span>
+      )}
     </Link>
   );
 };
