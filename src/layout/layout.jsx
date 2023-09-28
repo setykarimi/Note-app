ca382349267eb9d1a010878d8fb39d9fba3e68f0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 10:19:59 +0330

    Subject: Note app, Description: Edit style, Date: 9/28/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Sidebar from './sidebar'
+import Sidebar from './sidebar/sidebar'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div className='container mx-auto grid grid-cols-4'>
+    <div className='container mx-auto grid grid-cols-4 gap-8 p-4'>
         <Sidebar />
+        <Outlet />
     </div>
   )
 }
