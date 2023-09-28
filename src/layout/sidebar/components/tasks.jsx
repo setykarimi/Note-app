236ae3dc1384e95cbbbec4350f400988ebbd4ccf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 11:54:44 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 9/28/2023

--- src/layout/sidebar/components/tasks.jsx
+++ src/layout/sidebar/components/tasks.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import {
-  BiChevronsRight,
-  BiListCheck,
-  BiSolidNote
-} from "react-icons/bi";
-import { FaCalendarAlt } from 'react-icons/fa';
+import { BiChevronsRight, BiListCheck, BiSolidNote } from "react-icons/bi";
+import { FaCalendarAlt } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 
-export default function Tasks() {
+export default function Tasks({ expanded }) {
   const { pathname } = useLocation();
   const links = [
     {
       title: "Upcoming",
-      icon: <BiChevronsRight size={22} className="text-gray-500"/>,
+      icon: <BiChevronsRight size={22} className="text-gray-500" />,
       to: "/upcoming",
       badge: 12,
     },
@@ -35,32 +31,44 @@ export default function Tasks() {
   ];
 
   return (
-    <div className="mt-8 border-b border-b-gray-200 pb-4">
-      <span className="text-xs text-gray-600 font-bold">TASKS</span>
+    <div
+      className={`${
+        expanded ? "mt-8" : "mt-4"
+      }  border-b border-b-gray-200 pb-4`}
+    >
+      <span
+        className={`text-xs text-gray-600 font-bold block ${
+          expanded ? "text-left" : "text-center"
+        }`}
+      >
+        TASKS
+      </span>
       <div className="flex flex-col  mt-2">
         {links.map((link) => (
-          <CustomLink link={link} currentPath={pathname} />
+          <CustomLink link={link} currentPath={pathname} expanded={expanded} />
         ))}
       </div>
     </div>
   );
 }
 
-const CustomLink = ({ link, currentPath }) => {
+const CustomLink = ({ link, currentPath, expanded }) => {
   const path = link.to == currentPath;
   return (
     <Link
       to={link.to}
       className={`${
         path && "bg-gray-200 font-bold"
-      } p-2 rounded-md text-gray-600 flex items-center`}
+      } p-2 rounded-md text-gray-600 flex items-center ${
+        !expanded && "justify-center"
+      }`}
     >
       <span className="flex items-center gap-2">
         <span className="block w-5">{link.icon}</span>
-        {link.title}
+        {expanded && link.title}
       </span>
 
-      {link.badge && (
+      {link.badge && expanded && (
         <span
           className={`block ml-auto ${
             path ? "bg-white" : "bg-gray-200"
