8b9967fbd27b65b9e43dd58c7630e63921d99762
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 11:54:29 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 9/28/2023

--- src/layout/sidebar/components/otherlinks.jsx
+++ src/layout/sidebar/components/otherlinks.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { VscSettings } from "react-icons/vsc";
 import { FaSignOutAlt } from "react-icons/fa";
+import { VscSettings } from "react-icons/vsc";
+import { Link } from "react-router-dom";
 
-export default function OtherLinks() {
+export default function OtherLinks({ expanded }) {
   return (
     <div className="mt-auto">
       <Link
         to="/setting"
-        className={`px-2 py-1 rounded-md text-gray-600 flex items-center gap-2 mt-4`}
+        className={`px-2 py-1 rounded-md text-gray-600 flex items-center ${
+          !expanded ? "justify-center" : ""
+        } gap-2 mt-4`}
       >
-        <span className="block w-5 rotate-90">
+        <span className={`block rotate-90 ${expanded ? "w-5" : ""}`}>
           <VscSettings size={15} />
         </span>
-        Settings
+        {expanded && "Settings"}
       </Link>
       <Link
         to=""
-        className={`px-2 py-1 rounded-md text-gray-600 flex items-center gap-2`}
+        className={`px-2 py-1 rounded-md text-gray-600 flex items-center ${
+          !expanded ? "justify-center" : ""
+        } gap-2`}
       >
-        <span className="block w-5 ">
+        <span className={`block ${expanded ? "w-5" : ""}`}>
           <FaSignOutAlt size={15} />
         </span>
-        Sign out
+        {expanded && "Sign out"}
       </Link>
     </div>
   );
