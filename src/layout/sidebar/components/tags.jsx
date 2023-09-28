a23b1ac27c046d9e8747ea18aa5639c9b3e219a0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-28 11:54:38 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 9/28/2023

--- src/layout/sidebar/components/tags.jsx
+++ src/layout/sidebar/components/tags.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoMdAdd } from "react-icons/io";
 
-export default function Tags() {
+export default function Tags({ expanded }) {
   const tags = [
     {
       name: "Tag 1",
@@ -14,19 +14,46 @@ export default function Tags() {
   ];
   return (
     <div className="mt-4">
-      <span className="text-xs text-gray-600 font-bold">TAGS</span>
-      <div className="flex flex-wrap gap-1 mt-2">
-        {tags.map((tag)=> <Tag tag={tag} />)}
-        <button className="flex gap-1 items-center bg-gray-200 py-[6px] px-4 text-sm rounded-md font-bold text-gray-600">
-        <IoMdAdd className="text-gray-500" size={15}  />
-
-           Add Tag
+      <span
+        className={`text-xs text-gray-600 font-bold block ${
+          expanded ? "text-left" : "text-center"
+        }`}
+      >
+        TAGS
+      </span>
+      <div
+        className={`flex ${
+          expanded ? "flex-row" : "flex-col items-center"
+        } flex-wrap gap-1 mt-2`}
+      >
+        {tags.map((tag) => (
+          <Tag tag={tag} expanded={expanded} />
+        ))}
+        <button
+          className={`flex gap-1 items-center bg-gray-200 ${
+            expanded
+              ? "text-sm py-[6px] px-4"
+              : "text-xs p-1 w-full justify-center"
+          } text-sm rounded-md font-bold text-gray-600`}
+        >
+          <IoMdAdd className="text-gray-500" size={15} />
+          {expanded && "Add Tag"}
         </button>
       </div>
     </div>
   );
 }
 
-const Tag = ({ tag }) => {
-  return <span className={`block py-[6px] px-4 text-sm rounded-md font-bold text-gray-600 ${tag.color}`}>{tag.name}</span>;
+const Tag = ({ tag, expanded }) => {
+  return (
+    <span
+      className={`block  ${
+        expanded
+          ? "text-sm py-[6px] px-4 w-fit rounded-md"
+          : "text-xs p-1 w-full rounded text-center"
+      }  font-bold text-gray-600 ${tag.color}`}
+    >
+      {tag.name}
+    </span>
+  );
 };
