eea4bc77e8de0a69b7c08b777b8e9446a74a6c0e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-29 21:53:48 +0330

    Subject: Note app, Description: Edit for showing data, Date: 9/29/2023

--- src/components/tasks/task.jsx
+++ src/components/tasks/task.jsx
@@ -1,25 +1,38 @@
 import { HiChevronRight } from "react-icons/hi";
-import { BiSolidCalendarX  } from "react-icons/bi";
+import { BiSolidCalendarX } from "react-icons/bi";
 
-export default function Task({ task }) {
-  const showTaskDetails = (task) => {
+export default function Task({ onComplete, task }) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   };
+  
   return (
-    <div
-      className="p-4 cursor-pointer text-gray-600 border-b border-b-gray-150 flex justify-between items-center"
-      onClick={() => showTaskDetails(task)}
-    >
+    <div className="p-4 cursor-pointer text-gray-600 border-b border-b-gray-150 flex justify-between items-center">
       <div>
         <div className="flex items-center gap-2">
-          <input type="checkbox" className="w-4 h-4 border border-gray-100" />
-          <span>{task.title}</span>
+          <input
+            type="checkbox"
+            className="w-4 h-4 border border-gray-100"
+            value={task.id}
+            onChange={onComplete}
+          />
+          <div className={`flex items-center gap-1 ${task.completed ? "line-through	" : ""}`}>
+              {task.title}
+            {task.desc && (
+              <span className="text-xs font-light">({task.desc})</span>
+            )}
+          </div>
         </div>
         {(task.date || task.label) && (
           <div className="flex items-center flex-wrap gap-6 mt-2 ml-6">
             {task.date && (
               <div className="flex items-center text-gray-600 gap-1">
                 <BiSolidCalendarX size={14} />
-                <span className="text-xs  font-bold">{task.date}</span>
+                <span className="text-xs  font-bold">
+                  {new Date(task.date).toLocaleDateString("en-Us", options)}
+                </span>
               </div>
             )}
 
@@ -35,7 +48,7 @@ export default function Task({ task }) {
           </div>
         )}
       </div>
-      <HiChevronRight size={22} />
+      {/* <HiChevronRight size={22} /> */}
     </div>
   );
 }
