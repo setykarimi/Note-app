51dff707ec8371ccca7b74e1a4c819e601405fc9
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-09-30 21:27:13 +0330

    Subject: Note app, Description: Edit style, Date: 9/30/2023

--- src/components/tasks/taskList.jsx
+++ src/components/tasks/taskList.jsx
@@ -62,7 +62,7 @@ export default function TaskList() {
   };
 
   return (
-    <div className="grid grid-cols-3 gap-6">
+    <div className="grid lg:grid-cols-3 gap-6">
       <div className={showTaskForm ? "col-span-2" : "col-span-3"}>
         <button
           className="border border-gray-150 px-6 py-4 text-gray-400 rounded-lg w-full text-left flex items-center gap-2"
