bff3d1f275142ee86f7f452ce578b6443869805b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-07 21:53:40 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 10/7/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -8,11 +8,11 @@ export default function Layout() {
   return (
     <div
       className={`xl:container mx-auto  ${
-        expanded ? "md:grid md:grid-cols-4" : "flex"
-      } md:gap-8 gap-4 p-4 transition-all`}
+        expanded ? "md:grid xl:grid-cols-4 md:grid-cols-3 md:gap-8 gap-4" : "flex gap-2"
+      }  p-4 transition-all`}
     >
       <Sidebar expanded={expanded} setExpanded={setExpanded} />
-      <div className={`${expanded ? 'col-span-3' : "w-full"}`}>
+      <div className={`${expanded ? 'xl:col-span-3  md:col-span-2' : "w-full"}`}>
         <Outlet />
       </div>
     </div>
