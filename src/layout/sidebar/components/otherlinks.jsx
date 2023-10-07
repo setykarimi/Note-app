eeff9c136006d4a1993977ef89728e37ded42164
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-07 21:54:54 +0330

    Subject: Note app, Description: Edit for responsive view, Date: 10/7/2023

--- src/layout/sidebar/components/otherlinks.jsx
+++ src/layout/sidebar/components/otherlinks.jsx
@@ -3,30 +3,26 @@ import { FaSignOutAlt } from "react-icons/fa";
 import { VscSettings } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
-export default function OtherLinks({ expanded }) {
+export default function OtherLinks() {
   return (
     <div className="mt-auto">
       <Link
         to="/setting"
-        className={`px-2 py-1 rounded-md text-gray-600 flex items-center ${
-          !expanded ? "justify-center" : ""
-        } gap-2 mt-4`}
+        className="px-2 py-1 rounded-md text-gray-600 flex items-center gap-2 mt-4"
       >
-        <span className={`block rotate-90 ${expanded ? "w-5" : ""}`}>
+        <span className="block rotate-90 w-5">
           <VscSettings size={15} />
         </span>
-        {expanded && "Settings"}
+        Settings
       </Link>
       <Link
         to=""
-        className={`px-2 py-1 rounded-md text-gray-600 flex items-center ${
-          !expanded ? "justify-center" : ""
-        } gap-2`}
+        className="px-2 py-1 rounded-md text-gray-600 flex items-center gap-2"
       >
-        <span className={`block ${expanded ? "w-5" : ""}`}>
+        <span className="block w-5">
           <FaSignOutAlt size={15} />
         </span>
-        {expanded && "Sign out"}
+        Sign out
       </Link>
     </div>
   );
